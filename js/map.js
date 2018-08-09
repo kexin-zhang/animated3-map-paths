b27fb1ead0a15c9ff54e0d99815c2f9cd2beb2ac
Kexin Zhang <kxin.zhang@gmail.com>
2018-08-08 23:00:59 -0400

    add all animated paths for basic vis

---
+++ js/map.js
@@ -0,0 +1,120 @@
+class AnimatedMap {
+    constructor(div_id, start_coord, zoom, reset) {
+        this.div_id = div_id;
+        this.circle = null;
+
+        this.map = L.map(div_id, {zoomControl: false });
+        this.map.setView(start_coord, zoom);
+
+        // disable panning + zooming
+        this.map.dragging.disable();
+        this.map.touchZoom.disable();
+        this.map.doubleClickZoom.disable();
+        this.map.scrollWheelZoom.disable();
+        this.map.boxZoom.disable();
+        this.map.keyboard.disable();
+
+        // add tiles
+        let tiles = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+            subdomains: 'abcd',
+            maxZoom: 18
+        });
+        tiles.addTo(this.map);
+
+        // hit that reset function on zoom
+        this.map.on("viewreset moveend", reset);
+
+        // add d3 overlay
+        this.svg = d3.select(this.map.getPanes().overlayPane).append("svg")
+                     .attr("id", "overlay")
+                     .attr("class", "leaflet-zoom-hide")
+                     .style("width", this.map.getSize().x + "px")
+                     .style("height", this.map.getSize().y + "px");
+
+        this.g = this.svg.append("g");
+    }
+
+    addPath(filename, classname) {
+        let map = this.map;
+        let g = this.g;
+
+        let transform = d3.geoTransform({point: function(x, y) {
+                let point = map.latLngToLayerPoint(new L.LatLng(y, x));
+                this.stream.point(point.x, point.y);
+            }
+        });
+        let path = d3.geoPath().projection(transform);
+
+        d3.json(filename, function(error, data) {
+            if (error) throw error;
+
+            let coordinates = data.features; 
+
+            g.selectAll(`.${classname}`)
+             .data(coordinates)
+             .enter()
+             .append("path")
+             .attr("class", classname)
+             .attr("d", path);     
+        });
+    }
+
+    addAnimatedPath(filename, classname, duration, callback) {
+        let map = this.map;
+        let g = this.g;
+        let circle = this.circle;
+
+        let transform = d3.geoTransform({point: function(x, y) {
+                let point = map.latLngToLayerPoint(new L.LatLng(y, x));
+                this.stream.point(point.x, point.y);
+            }
+        });
+        let path = d3.geoPath().projection(transform);
+
+        d3.json(filename, function(error, data) {
+            if (error) throw error;
+
+            let coordinates = data.features; 
+
+            let line = g.selectAll(`.${classname}`)
+                             .data(coordinates)
+                             .enter()
+                             .append("path")
+                             .attr("class", classname)
+                             .attr("d", path);
+
+            line.transition()
+                .duration(duration)
+                .attrTween("stroke-dasharray", function() {
+                    return function(t) {
+                        // for stroke dash array interpolation 
+                        // https://bl.ocks.org/mbostock/5649592
+    
+                        let l = line.node().getTotalLength(),
+                            i = d3.interpolateString("0," + l, l + "," + l);
+                        
+                        if (circle) {
+                            let p = line.node().getPointAtLength(t * l);
+                            circle.attr("transform", "translate(" + p.x + "," + p.y + ")")
+                        }
+                        
+                        return i(t);
+                    }
+                })
+                .on("end", callback);
+        });
+    }
+
+    initCircle(coords, classname, radius) {
+        let point = this.map.latLngToLayerPoint(new L.LatLng(coords[1], coords[0]));
+
+        this.circle = this.g.append("circle")
+                            .attr("cx", 0)
+                            .attr("cy", 0)
+                            .attr("transform", "translate(" + point.x + "," + point.y + ")")
+                            .attr("r", radius)
+                            .attr("class", classname);
+    }
+
+}; 
---
+++ js/script.js
@@ -0,0 +1,85 @@
+const small_radius = 7;
+const medium_radius = 8;
+const large_radius = 10;
+
+const zoomStates = {
+    HIKE: "hike",
+    FOOD_DRIVE: "food_drive",
+    RETURN_DRIVE: "return_drive"
+};
+
+let currentZoomState = null;
+
+// this gets called whenever the map zoom level is reset
+function zoomReset() {
+    if (currentZoomState === zoomStates.HIKE) {
+        onp.addPath("./data/onp_drive1.geojson", "drive");
+        onp.initCircle([-123.32757, 47.51479], "hike-circle", large_radius);
+        plotHike();
+    }
+
+    if(currentZoomState === zoomStates.FOOD_DRIVE) {
+        plotFoodDrive();
+    }
+
+    if(currentZoomState == zoomStates.RETURN_DRIVE) {
+        plotReturnDrive();
+    }
+}
+
+let onp = new AnimatedMap("map", [47.2995, -122.6207], 9, zoomReset);
+onp.initCircle([-122.33744, 47.61685], "drive-circle", small_radius);
+onp.addAnimatedPath("./data/onp_drive1.geojson", "drive", 16000, function() {
+    d3.selectAll(".drive").remove();
+    d3.selectAll("circle").remove();
+    currentZoomState = zoomStates.HIKE;
+    onp.map.flyTo([47.512171, -123.323818], 14);
+});
+
+function plotHike() {
+    onp.addAnimatedPath("./data/onp_hike.geojson", "hike", 7000, plotLakeDrive);
+}
+
+function plotLakeDrive() {
+    onp.circle.attr("class", "drive-circle");
+    onp.addAnimatedPath("./data/onp_drive2.geojson", "lake-drive", 5000, plotRaft);
+}
+
+function plotRaft() {
+    onp.circle.attr("class", "raft-circle");
+    onp.addAnimatedPath("./data/onp_raft.geojson", "raft", 8000, function() {
+        d3.selectAll("path").remove();
+        d3.selectAll("circle").remove();
+        currentZoomState = zoomStates.FOOD_DRIVE;
+        onp.map.flyTo([47.466555, -123.219021], 12);
+    });
+}
+
+function plotFoodDrive() {
+    // onp.addPath("./data/onp_drive1.geojson", "drive");
+    onp.addPath("./data/onp_hike.geojson", "hike");
+    onp.addPath("./data/onp_drive2.geojson", "lake-drive");
+    onp.addPath("./data/onp_raft.geojson", "raft");
+
+    onp.initCircle([-123.31544, 47.50567], "drive-circle", medium_radius);
+    onp.addAnimatedPath("./data/onp_drive3.geojson", "drive", 5000, plotCampDrive);
+}
+
+function plotCampDrive() {
+    onp.addAnimatedPath("./data/onp_drive4.geojson", "camp-drive", 5000, function() {
+        d3.selectAll("path").remove();
+        d3.selectAll("circle").remove();
+        currentZoomState = zoomStates.RETURN_DRIVE;
+        onp.map.flyTo([47.2995, -122.6207], 9);
+    });
+}
+
+function plotReturnDrive() {
+    // onp.addPath("./data/onp_drive1.geojson", "drive");
+    onp.addPath("./data/onp_hike.geojson", "hike");
+    onp.addPath("./data/onp_drive2.geojson", "lake-drive");
+    onp.addPath("./data/onp_raft.geojson", "raft");
+
+    onp.initCircle([-123.32757, 47.51479], "drive-circle", small_radius);
+    onp.addAnimatedPath("./data/onp_drive5.geojson", "drive", 16000, null);
+}
